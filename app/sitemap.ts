f7f0826f04740caae5d1ae52ec87beebbe753cf9
IxiAngel <33629337+IxiAngel@users.noreply.github.com>
2026-05-21 20:52:15 +0200

    sitemap fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,7 +7,7 @@ export const dynamic = "force-static"
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return PageRoutes.map((page) => ({
-    url: `${Settings.metadataBase}${page.href}`,
+    url: `${Settings.metadataBase}/docs${page.href}`,
     lastModified: new Date().toISOString(),
     changeFrequency: "monthly",
     priority: 0.8,
